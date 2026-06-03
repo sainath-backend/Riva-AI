260bf3e6a454dbde2a9e9daf11755d150b196d0d
sainath tingilkar <tingilkarsainath0@gmail.com>
2026-06-03 13:11:47 +0530

    Setup Vite frontend and create authentication UI

---
+++ Client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path='/' element= {<Home/>} />
+      <Route path='/login' element= {<Login/>} />
+    </Routes>
+    </>
+  )
+}
+
+export default App
--- Server/index.js
+++ Server/index.js
@@ -4,9 +4,13 @@ import connectDB from "./Configs/ConnectDB.js";
 import authRouter from "./Routes/auth.route.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
+import cors from "cors"
 
 const app = express();
-
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 app.use(express.json());
 app.use(cookieParser());
 
